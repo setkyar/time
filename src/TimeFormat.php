ee95691bd4998305512f2ac06eb3e0ecb4a5dc91
setkyar <setkyar16@gmail.com>
2015-03-06 11:58:10 +0630

    Initialized PSR4 with PHPUnit

---
+++ src/TimeFormat.php
@@ -0,0 +1,18 @@
+<?php namespace SetKyar\TimeFormat;
+
+/**
+* @author Set Kyar Wa Lar
+*/
+class TimeFormat
+{
+	/**
+	 * undocumented function
+	 *
+	 * @return void
+	 * @author Set Kyar Wa Lar
+	 **/
+	public function FunctionName($value='')
+	{
+		# code...
+	}
+}
